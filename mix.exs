8cfc737d4dddd81a90b382b0bc2e43a0db223566
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2023-05-09 19:53:13 +0200

    Added step to install assets

--- mix.exs
+++ mix.exs
@@ -69,7 +69,12 @@ defmodule Demonstration.MixProject do
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
       "assets.build": ["tailwind default", "esbuild default"],
-      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
+      "assets.deploy": [
+        "cmd --cd assets npm install",
+        "tailwind default --minify",
+        "esbuild default --minify",
+        "phx.digest"
+      ]
     ]
   end
 end
